8294370862e5723fb01aac4a57c690702044377e
Robin Scheibler <robin.scheibler@linecorp.com>
2021-08-06 09:52:37 +0900

    Fixes new FNet implementation

--- torchiva/models/fnet.py
+++ torchiva/models/fnet.py
@@ -6,17 +6,69 @@ from .base import SourceModelBase
 from torchaudio.transforms import MelScale
 
 
-class FeedForward(nn.Module):
-    def __init__(self, num_features, expansion_factor, dropout, dtype=None):
+class ComplexDropout(nn.Module):
+    def __init__(self, p):
         super().__init__()
-        num_hidden = expansion_factor * num_features
-        self.fc1 = nn.Linear(num_features, num_hidden, dtype=dtype)
-        self.fc2 = nn.Linear(num_hidden, num_features, dtype=dtype)
-        self.dropout1 = nn.Dropout(dropout)
-        self.dropout2 = nn.Dropout(dropout)
+        self.dropout = nn.Dropout(p)
 
     def forward(self, x):
-        x = self.dropout1(F.gelu(self.fc1(x)))
+        if x.dtype in [torch.complex64, torch.complex128]:
+            x = torch.view_as_real(x)
+            x = self.dropout(x)
+            return torch.view_as_complex(x)
+        else:
+            return self.dropout(x)
+
+
+class ComplexLinear(nn.Module):
+    def __init__(self, num_input, num_output, **kwargs):
+        super().__init__()
+        self.real = nn.Linear(num_input, num_output, **kwargs)
+        self.imag = nn.Linear(num_input, num_output, **kwargs)
+
+    def forward(self, x):
+
+        if x.dtype in [torch.complex64, torch.complex128]:
+            real = self.real(x.real) - self.imag(x.imag)
+            imag = self.real(x.imag) + self.imag(x.real)
+        else:
+            real = self.real(x)
+            imag = self.imag(x)
+
+        out = torch.view_as_complex(
+            torch.cat([real[..., None], imag[..., None]], dim=-1)
+        )
+        return out
+
+
+def complex_non_linearity(x):
+    mod = torch.clamp(x.abs(), min=1e-5)
+    G = torch.sqrt(mod) / mod
+    return G * x
+
+
+class FeedForward(nn.Module):
+    def __init__(self, num_features, expansion_factor, dropout, complex=False):
+        super().__init__()
+        num_hidden = expansion_factor * num_features
+        self.complex = complex
+        if complex:
+            self.fc1 = ComplexLinear(num_features, num_hidden)
+            self.fc2 = ComplexLinear(num_hidden, num_features)
+        else:
+            self.fc1 = nn.Linear(num_features, num_hidden)
+            self.fc2 = nn.Linear(num_hidden, num_features)
+        self.dropout1 = ComplexDropout(dropout)
+        self.dropout2 = ComplexDropout(dropout)
+
+    def forward(self, x):
+        x = self.fc1(x)
+
+        if self.complex and x.dtype in [torch.complex64, torch.complex128]:
+            x = complex_non_linearity(x)
+        else:
+            x = F.gelu(x)
+        x = self.dropout1(x)
         out = self.dropout2(self.fc2(x))
         return out
 
@@ -28,19 +80,18 @@ def fourier_transform(x):
 class FNetEncoderLayer(nn.Module):
     def __init__(self, d_model, expansion_factor, dropout):
         super().__init__()
-        self.ff_freq = FeedForward(
-            d_model, expansion_factor, dropout, dtype=torch.complex64
-        )
+        self.ff_freq = FeedForward(d_model, expansion_factor, dropout, complex=True)
         self.ff_time = FeedForward(d_model, expansion_factor, dropout)
-        self.norm1 = nn.LayerNorm(d_model)
+        # self.norm1 = nn.LayerNorm(d_model)
         self.norm2 = nn.LayerNorm(d_model)
 
     def forward(self, x):
         residual = x
-        x = torch.fft.rfft(x, dim=-2)
+        n = x.shape[-2]
+        x = torch.fft.rfft(x, dim=-2, n=n)
         x = self.ff_freq(x)
-        x = self.norm1(x)
-        x = torch.fft.irfft(x)
+        # x = self.norm1(x)
+        x = torch.fft.irfft(x, dim=-2, n=n)
         x = self.ff_time(x)
         out = self.norm2(x + residual)
         return out
@@ -68,7 +119,6 @@ class FNetModel(SourceModelBase):
         self,
         n_freq=257,
         n_mels=64,
-        num_features=256,
         expansion_factor=2,
         dropout=0.5,
         num_layers=6,
@@ -79,7 +129,7 @@ class FNetModel(SourceModelBase):
         self.eps = eps
 
         self.fnet = FNet(
-            d_model=num_features,
+            d_model=n_mels,
             expansion_factor=expansion_factor,
             dropout=dropout,
             num_layers=num_layers,
